e4f79beed47c56062689a9bb470c2d730baf5f13
jamibear <5701ja.mi@gmail.com>
2024-02-18 18:11:49 +0800

    set wrap to false

--- config/options.nix
+++ config/options.nix
@@ -4,6 +4,7 @@
 		      number = true;         # Show line numbers
 		      relativenumber = true; # Show relative line numbers
 		      shiftwidth = 4;        # Tab width should be 2
+		      wrap = false;
 		};
 	};
 }
