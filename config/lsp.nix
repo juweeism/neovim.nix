404353105e16e94b943becc26bd5623d35ae10ad
jamibear <5701ja.mi@gmail.com>
2024-02-24 21:19:36 +0800

    add formatting and lsp plugins

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,16 +1,36 @@
-# LANGUAGE SERVERS 
-{
-    plugins.lsp = {
-        enable = true;
+# LANGUAGE SERVERS
+# FORMATTERS TOO
 
-	servers = {
-	    tsserver.enable = true;
-	    nixd.enable = true;
-	    
-	    lua-ls = {
-	        enable = true;
-	        settings.telemetry.enable = false;
-	    };
-	};
+{
+  plugins.lsp = {
+    enable = true;
+
+    servers = {
+      astro.enable = true;
+      tsserver.enable = true;
+      nixd.enable = true;
+
+      lua-ls = {
+        enable = true;
+        settings.telemetry.enable = false;
+      };
     };
+  };
+
+  plugins.none-ls = {
+    enable = true;
+    enableLspFormat = true;
+
+    sources = {
+      formatting = {
+        nixfmt.enable = true;
+        nixpkgs_fmt.enable = true;
+        prettierd.enable = true;
+      };
+
+      code_actions = { eslint_d.enable = true; };
+    };
+  };
+
+  plugins.lsp-format.enable = true;
 }
