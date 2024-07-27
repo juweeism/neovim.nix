288bf48c8994c344b6e158d0e980199d0e360b9d
MJ <mijane.404@gmail.com>
2024-07-27 18:58:27 +0800

    fillchar try

--- config/options.nix
+++ config/options.nix
@@ -1,14 +1,17 @@
 {
 	config = {
 		options = {
-		      number = true;         
-		      relativenumber = true; 
-		      shiftwidth = 4;        
-		      tabstop = 4;        
-		      wrap = false;
-		      expandtab = true;
-		      signcolumn = "number";
-		      cursorline = true;
+			number = true;         
+			relativenumber = true; 
+			shiftwidth = 4;        
+			tabstop = 4;        
+			wrap = false;
+		    expandtab = true;
+		    signcolumn = "number";
+		    cursorline = true;
+			fillchars = {
+				eob = "·"
+			};
 		};
 	};
 }
