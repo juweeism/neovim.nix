c61c5c369bb021ae4375674fac48ae70e1d404e6
MJ <mijane.404@gmail.com>
2024-07-27 14:39:36 +0800

    oops add to settings

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -2,15 +2,17 @@
     plugins.treesitter = {
         enable = true;
 	ensureInstalled = "all";
+
+	settings = {
+	    highlight = {
+	    	enable = true;
+	    	additional_vim_regex_highlighting = true;
+	    	custom_captures = { };
+  	    };
 	
-	highlight = {
-	    enable = true;
-	    additional_vim_regex_highlighting = true;
-	    custom_captures = { };
-  	};
-	
-	indent = {
-	    enable = true;
+	    indent = {
+	    	enable = true;
+	    };
 	};
     };
 }
