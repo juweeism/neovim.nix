871c14c9dfe8edaf511e90694836de4e482a8aa4
MJ <mijane.404@gmail.com>
2024-07-27 14:35:44 +0800

    enable highlighter

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -2,5 +2,14 @@
     plugins.treesitter = {
         enable = true;
 	ensureInstalled = "all";
-    };
+	
+	highlight = {
+	    enable = true;
+	    additional_vim_regex_highlighting = true;
+	    custom_captures = { };
+  	};
+	
+	indent = {
+	    enable = true;
+	};
 }
