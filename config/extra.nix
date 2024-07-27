c5690165dff44856ce01233ccb04a19edb44dbfc
MJ <mijane.404@gmail.com>
2024-07-27 17:08:59 +0800

    last resort

--- config/extra.nix
+++ config/extra.nix
@@ -4,27 +4,33 @@
 
 {
     extraPlugins = with pkgs.vimPlugins; [
-      lightline-bufferline
-      lightline-gruvbox-vim
-      limelight-vim
-      plenary-nvim # required for telescope
-      haskell-tools-nvim
+        lightline-bufferline
+        lightline-gruvbox-vim
+        limelight-vim
+        plenary-nvim # required for telescope
+        haskell-tools-nvim
     ];
 
     extraConfigLua = ''
 	require('lualine').setup {
 	    tabline = {
-	      lualine_a = {'buffers'},
-	      lualine_b = {},
-	      lualine_c = {},
-	      lualine_x = {},
-	      lualine_y = {},
-	      lualine_z = {}
+            lualine_a = {'buffers'},
+	        lualine_b = {},
+	        lualine_c = {},
+	        lualine_x = {},
+	        lualine_y = {},
+	        lualine_z = {}
 	    }
 	}
 
 	local ht = require('haskell-tools')
 	--- Start or attach the LSP client.
 	ht.lsp.start()
+
+	require'nvim-treesitter.configs'.setup {
+        highlight = {
+            enable = true,
+        }
+    }
     '';
 }
