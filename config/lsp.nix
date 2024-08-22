51586188e51f9d8552c8e514acb32fa259f6f7c9
MJ <mijane.404@gmail.com>
2024-08-22 22:27:44 +0800

    try enable clangd config

--- config/lsp.nix
+++ config/lsp.nix
@@ -5,6 +5,7 @@
 
         servers = {
             clangd.enable = true;
+            clangd.cmd = [ 'clangd --enable-config' ];
             cmake.enable = true;
             volar.enable = true;
             tsserver.enable = true;
